60c93a6efc1cfa06f55177b53d608f57af442fb3
skyeome <tjru7230@naver.com>
2024-06-03 19:17:43 +0900

    #8.1 Database Validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -4,6 +4,7 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
 
 const checkUsername = (username: string) => !username.includes("potato");
@@ -16,6 +17,23 @@ const checkPassword = ({
   confirm_password: string;
 }) => password === confirm_password;
 
+const checkUniqueUsername = async (username: string) => {
+  // username이 중복인지 확인
+  const user = await db.user.findUnique({
+    where: { username },
+    select: { id: true },
+  });
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -25,8 +43,14 @@ const formSchema = z
       })
       .toLowerCase()
       .trim()
-      .refine(checkUsername, "potato는 사용할수 없습니다."),
-    email: z.string().email().toLowerCase().trim(),
+      .refine(checkUsername, "potato는 사용할수 없습니다.")
+      .refine(checkUniqueUsername, "이미 존재하는 사용자명 입니다."),
+    email: z
+      .string()
+      .email()
+      .toLowerCase()
+      .trim()
+      .refine(checkUniqueEmail, "이미 존재하는 이메일 입니다."),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH)
@@ -46,10 +70,13 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get("confirm_password"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // TODO: 회원가입 후 로그인 로직 작성
+    // 비밀번호 암호화
+    // 사용자 db에 저장
+    // redirect "/home"
   }
 }
