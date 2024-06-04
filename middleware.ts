826be490232bd5791fe7b7285a5bc9dfa3aa84ba
skyeome <tjru7230@naver.com>
2024-06-04 15:57:19 +0900

    #8.12 Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,33 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-  console.log("middleware");
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    // 로그인 안한 상태
+    if (!exists) {
+      // 로그인이 필요한 페이지인 경우
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    // 이미 로그인 한 상태
+    if (exists) {
+      // 로그인, 회원가입 관련 페이지 일때
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
 }
 
 export const config = {
