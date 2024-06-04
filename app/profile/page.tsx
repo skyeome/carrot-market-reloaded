e768da4518c223458d93249a12af527760148cc6
skyeome <tjru7230@naver.com>
2024-06-04 15:04:07 +0900

    #8.7 Log Out

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,35 @@
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { notFound, redirect } from "next/navigation";
 import React from "react";
 
-export default function Profile() {
-  return <div>welcome to your profile</div>;
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) return user;
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+    const session = await getSession();
+    await session.destroy();
+    redirect("/");
+  };
+  return (
+    <div>
+      <h1>Welcome! {user?.username}</h1>
+      <form action={logOut}>
+        <button>Log out</button>
+      </form>
+    </div>
+  );
 }
