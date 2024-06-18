d432f8c66cf0dcf36d7182d66d146a4d83b2de9c
skyeome <tjru7230@naver.com>
2024-06-18 14:13:39 +0900

    #10.3 Product Component

--- app/(tabs)/products/loading.tsx
+++ app/(tabs)/products/loading.tsx
@@ -5,7 +5,7 @@ export default function ProductsLoading() {
     <div className="p-5 animate-pulse flex flex-col gap-5">
       {[...Array(10)].map((_, index) => (
         <div key={index} className="*:rounded-md flex gap-5">
-          <div className="size-28 bg-neutral-700" />
+          <div className="size-28 bg-neutral-700 flex-shrink-0" />
           <div className="flex flex-col gap-2 *:rounded-md">
             <div className="bg-neutral-700 h-5 w-40" />
             <div className="bg-neutral-700 h-5 w-20" />
