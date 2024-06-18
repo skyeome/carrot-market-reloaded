2a33b92940256dd3fe2c248fdb197cee6032ccea
skyeome <tjru7230@naver.com>
2024-06-18 12:29:34 +0900

    #10.2 Skeletons

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,3 +1,8 @@
-export default function Products() {
+async function getProducts() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+}
+
+export default async function Products() {
+  const products = await getProducts();
   return <div>Products</div>;
 }
