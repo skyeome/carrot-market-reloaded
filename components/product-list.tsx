455a3e8b15c25f01fb246419b697075df672fc16
skyeome <tjru7230@naver.com>
2024-06-18 17:13:58 +0900

    #10.8 Recap

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -12,10 +12,17 @@ interface ProductListProps {
 export default function ProductList({ initialProducts }: ProductListProps) {
   const [products, setProducts] = useState(initialProducts);
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
   const onClickMore = async () => {
     setIsLoading(true);
-    const newProducts = await getMoreProducts(1);
-    setProducts((prev) => [...prev, ...newProducts]);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
     setIsLoading(false);
   };
   return (
@@ -23,13 +30,17 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-        disabled={isLoading}
-        onClick={onClickMore}
-      >
-        {isLoading ? "로딩 중" : "더 보기"}
-      </button>
+      {isLastPage ? (
+        <p>더 이상 상품이 없습니다.</p>
+      ) : (
+        <button
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+          disabled={isLoading}
+          onClick={onClickMore}
+        >
+          {isLoading ? "로딩 중" : "더 보기"}
+        </button>
+      )}
     </div>
   );
 }
