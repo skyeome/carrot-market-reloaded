b4a8d7cf42a1b36f4bf8619a4588efb37a60ae76
skyeome <tjru7230@naver.com>
2024-06-18 17:57:43 +0900

    #10.9 Infinite Scrolling

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import { InitialProducts } from "@/app/(tabs)/products/page";
 import ListProduct from "./list-product";
-import { useState } from "react";
 import { getMoreProducts } from "@/app/(tabs)/products/actions";
 
 interface ProductListProps {
@@ -14,33 +14,50 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
   const [isLastPage, setIsLastPage] = useState(false);
-  const onClickMore = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(page + 1);
-    if (newProducts.length !== 0) {
-      setPage((prev) => prev + 1);
-      setProducts((prev) => [...prev, ...newProducts]);
-    } else {
-      setIsLastPage(true);
+  const trigger = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (entries, observer) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page + 1);
+          if (newProducts.length !== 0) {
+            setProducts((prev) => [...prev, ...newProducts]);
+            setPage((prev) => prev + 1);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
+        }
+      },
+      {
+        threshold: 1.0,
+      }
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
     }
-    setIsLoading(false);
-  };
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
+
   return (
     <div className="p-5 flex flex-col gap-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      {isLastPage ? (
-        <p>더 이상 상품이 없습니다.</p>
-      ) : (
-        <button
+      {!isLastPage ? (
+        <span
+          ref={trigger}
           className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-          disabled={isLoading}
-          onClick={onClickMore}
         >
           {isLoading ? "로딩 중" : "더 보기"}
-        </button>
-      )}
+        </span>
+      ) : null}
     </div>
   );
 }
