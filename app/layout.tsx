eb1efcd54f3db4039ec2651c57efb0fbe4bf972d
skyeome <tjru7230@naver.com>
2024-06-28 12:55:03 +0900

    #12.4 Parallel Routes & Default Routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  // @ts-ignore
+  potato,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
@@ -19,6 +21,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-neutral-900 text-white max-w-screen-sm mx-auto`}
       >
+        {potato}
         {children}
       </body>
     </html>
