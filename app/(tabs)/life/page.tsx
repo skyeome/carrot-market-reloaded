6d0c811aacfb12cc0e34dd3d9ca43302b6fb8c93
skyeome <tjru7230@naver.com>
2024-07-03 17:18:19 +0900

    #14.1 See posts

--- app/(tabs)/life/page.tsx
+++ app/(tabs)/life/page.tsx
@@ -1,3 +1,51 @@
-export default function Life() {
-  return <div>Life</div>;
+import Link from "next/link";
+import { unstable_cache as nextCache } from "next/cache";
+import { formatToTimeAgo } from "@/lib/utils";
+import {
+  ChatBubbleBottomCenterIcon,
+  HandThumbUpIcon,
+} from "@heroicons/react/24/outline";
+import getPosts from "@/lib/posts/getPosts";
+
+export const metadata = {
+  title: "동네생활",
+};
+
+const getCachedProducts = nextCache(getPosts, ["posts"], {
+  revalidate: 60,
+});
+
+export default async function Life() {
+  const posts = await getCachedProducts();
+  return (
+    <div className="p-5 flex flex-col">
+      {posts.map((post) => (
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className="pb-5 mb-5 border-b border-neutral-500 text-neutral-400 flex  flex-col gap-2 last:pb-0 last:border-b-0"
+        >
+          <h2 className="text-white text-lg font-semibold">{post.title}</h2>
+          <p>{post.description}</p>
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex gap-4 items-center">
+              <span>{formatToTimeAgo(post.created_at.toString())}</span>
+              <span>·</span>
+              <span>조회 {post.views}</span>
+            </div>
+            <div className="flex gap-4 items-center *:flex *:gap-1 *:items-center">
+              <span>
+                <HandThumbUpIcon className="size-4" />
+                {post._count.likes}
+              </span>
+              <span>
+                <ChatBubbleBottomCenterIcon className="size-4" />
+                {post._count.comments}
+              </span>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
---
+++ lib/posts/getPosts.ts
@@ -0,0 +1,20 @@
+import db from "../db";
+
+export default async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      views: true,
+      created_at: true,
+      _count: {
+        select: {
+          comments: true,
+          likes: true,
+        },
+      },
+    },
+  });
+  return posts;
+}
